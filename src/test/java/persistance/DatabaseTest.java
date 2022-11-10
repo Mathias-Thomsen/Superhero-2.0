70ed330c03f50b20152c92e2b51316196a567e15
CarlHarlang <carl856k@stud.kea.dk>
2022-11-10 08:07:41 +0100

    Tests should now be done

---
+++ src/test/java/persistance/DatabaseTest.java
@@ -0,0 +1,37 @@
+package persistance;
+
+import Superhero.Superhero;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class DatabaseTest {
+    Database database = new Database();
+
+    @Test
+    void sortNameMethod() throws IOException {
+        database.sortSuperHeroRealName();
+        database.primaryNameSecondaryRealName();
+        database.sortNameMethod();
+    }
+    @Test
+    void sortSuperPower(){
+        database.sortSuperPower();
+        database.sortPower();
+    }
+    @Test
+    void sortCreationYear(){
+        database.sortCreationYear();
+    }
+    @Test
+    void sortIsHuman(){
+        database.sortIsHuman();
+    }
+
+
+}
